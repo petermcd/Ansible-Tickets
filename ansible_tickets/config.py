3f821223454011e26bf31c7d5df73e9253e91f03
Peter McDonald <me@petermcdonald.co.uk>
2021-02-15 15:57:36 +0000

    Implementing

---
+++ ansible_tickets/config.py
@@ -0,0 +1,28 @@
+from yaml import load
+
+
+class Config:
+    __slots__ = ['_conf']
+
+    def __init__(self, filename: str, path: str = '/usr/etc/opt/'):
+        """
+            Constructor
+
+            :param filename: The name of the config file
+            :param path: The path of the config file
+        """
+        file = f'{path}{filename}'
+        with open(file, 'r') as handler:
+            self._conf = load(handler)
+
+    def get(self, item: str) -> str:
+        """
+            Getter for config values
+
+            :param item: The config item required
+
+            :return: Value for config if exists otherwise an empty string
+        """
+        if item in self._conf:
+            return self._conf[item]
+        return ''
