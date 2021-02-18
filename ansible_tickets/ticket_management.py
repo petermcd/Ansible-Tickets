1dcaa2fcf9a35df7af4eb235d27354769ee4b860
Peter McDonald <me@petermcdonald.co.uk>
2021-02-18 19:11:24 +0000

    Implemented insight

--- ansible_tickets/ticket_management.py
+++ ansible_tickets/ticket_management.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import Dict, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from jira.resources import Issue
 from jira import JIRA
@@ -9,12 +9,14 @@ from ansible_tickets.config import Config
 
 
 class TicketManagement:
-    __slots__ = ['_jira', '_tickets']
+    __slots__ = ['_insight_field', '_insight_objects', '_jira', '_tickets']
 
     def __init__(self, config: Config):
         """
             Constructor
         """
+        self._insight_field = config.get('insight_field')
+        self._insight_objects: Dict[str, Any] = {}
         self._tickets: Dict[str, Issue] = {}
         self._jira = JIRA(
             config.get('jira_url'),
@@ -38,22 +40,32 @@ class TicketManagement:
         res = self.get_ticket(project, title)
         return bool(res)
 
-    def create_ticket(self, project: str, title: str, description: str, issue_type: str = 'Bug'):
+    def create_ticket(
+            self,
+            project: str,
+            title: str,
+            description: str,
+            insight_object_key: Optional[str],
+            issue_type: str = 'Bug'
+    ):
         """
             Creates a ticket in the given project
 
             :param project: Jira project the ticket should be created in
             :param title: The desired title of the ticket
             :param description: The description of the ticket
+            :param insight_object_key: ID of insight object to link to the ticket
             :param issue_type: Ticket type
         """
         logging.info(f'Creating ticket {title} in project {project} of type {issue_type}')
-        issue_dict = {
+        issue_dict: Dict[str, Union[str, Dict[str, str], List[Dict[str, str]]]] = {
             'project': {'key': project},
             'summary': title,
             'description': description,
             'issuetype': {'name': issue_type},
         }
+        if insight_object_key:
+            issue_dict[self._insight_field] = [{"key": f"{insight_object_key}"}]
         self._jira.create_issue(fields=issue_dict)
 
     def get_ticket(self, project: str, title: str) -> Optional[Issue]:
@@ -98,3 +110,22 @@ class TicketManagement:
         """
         logging.debug(f'Updating ticket {ticket.key} with comment "{comment}"')
         self._jira.add_comment(ticket.key, comment)
+
+    def get_insight_object_key(self, host: str) -> str:
+        """
+            Fetches Insight objects matching the IP
+
+            :param host: IP or Hostname of the object to retrieve
+
+            :return:
+        """
+        if host not in self._insight_objects:
+            fields = {
+                'iql': f'Hostname = {host} OR IP = {host}',
+                'resultPerPage': 1,
+            }
+            res = self._jira.iql(fields=fields)
+            if res.pageSize == 1:
+                self._insight_objects[host] = res.objectEntries[0].objectKey
+
+        return self._insight_objects.get(host, '')
